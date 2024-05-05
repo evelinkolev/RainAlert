141c6aada71ebcbe0e6a6ff8d9d496f75eaad41e
evelinkolev <evelinkolev@vivaldi.net>
2024-05-05 16:25:48 +0300

    Send sms via the Twilio API

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 import requests
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 MY_LAT = 42.696491
 MY_LON = 23.326010
 
 ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 API_KEY = "d10054c03a6fda2d717c1a48eb8bed6f"
+ACCOUNT_SID = "YOUR ACCOUNT SID"
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
 
 params = {
     "lat": MY_LAT,
@@ -24,5 +29,17 @@ for hour_data in weather_data["list"]:
     condition_code = hour_data["weather"][0]["id"]
     if int(condition_code) < 700:
         will_rain = True
+
 if will_rain:
-    print("Bring an umbrella.")
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(ACCOUNT_SID, AUTH_TOKEN, http_client=proxy_client)
+
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
